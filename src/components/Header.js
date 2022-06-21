8b60d32e46fe9a76853d7d03522cff9b4d332695
Matheus Santos <matheus.p.1996@hotmail.com>
2022-06-21 17:10:22 -0300

    Resolução requisito 1 - Complementando

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import PropType from 'prop-types';
 
 class Header extends React.Component {
   render() {
-    return (<header><h1>Sistema Solar</h1></header>);
+    return (
+      <header>
+        <h1>Sistema Solar</h1>
+      </header>);
   }
 }
+
+Header.PropType = PropType.string.isRequired;
 
 export default Header;
