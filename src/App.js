b8031f0b6133d9521692bc4e3ad35b081542e666
Matheus Santos <matheus.p.1996@hotmail.com>
2022-06-21 17:22:00 -0300

    Resolução requisito 2

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import Header from './components/Header';
+import SolarSystem from './components/SolarSystem';
 
 class App extends React.Component {
   render() {
-    return <Header />;
+    return (
+      <>
+        <Header />
+        <SolarSystem />
+      </>
+    );
   }
 }
 
